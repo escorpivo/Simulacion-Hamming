05c33aa3598ec20f62b4ae44b5dec767caf62b2f
LorenaAlbillos <lalbim00@estudiantes.unileon.es>
2024-03-18 13:52:30 +0100

    Update: decodificacion hecha, y fixed errores en la codificacion

--- src/sinRuido.java
+++ src/sinRuido.java
@@ -9,7 +9,7 @@ public class sinRuido {
 	static int longitud;
 	static int n;
 	static int k;
-
+	static int numEntero = 0;
 	public static void main(String[] args) {
 		
 		//Declaramos variables globales:
@@ -48,6 +48,13 @@ public class sinRuido {
 			System.out.println();
 		}
 		
+		//COD1 -- hacer el logaritmo en base 2 de la longitud del alfabeto
+		longitud = alfabeto.length();
+		double logaritmo = Math.log10(longitud)/Math.log10(2);
+		numEntero = (int) Math.round(logaritmo);
+		System.out.println("LOGARITMO Y SU ENTERO");
+		System.out.println(logaritmo);
+		System.out.println(numEntero);
 		
 		Scanner input = new Scanner(System.in);
 		System.out.println("1: Codificar		2: Decodificar");
@@ -82,16 +89,8 @@ public class sinRuido {
 	
 	//parte de codificar sin ruido
 	public static void codificar() {
-		longitud = alfabeto.length();
 		System.out.println(longitud);
 		System.out.println(alfabeto);
-		
-		//COD1 -- hacer el logaritmo en base 2 de la longitud del alfabeto
-		double logaritmo = Math.log10(longitud)/Math.log10(2);
-		int numEntero = (int) Math.round(logaritmo);
-		System.out.println("LOGARITMO Y SU ENTERO");
-		System.out.println(logaritmo);
-		System.out.println(numEntero);
 		
 		//Pedir la cadena que se va a codificar
 		System.out.println("Escriba la cadena a codificar");
@@ -124,26 +123,10 @@ public class sinRuido {
 			System.out.println(codificacion[i]);
 		}
 		
-		//Como nos van a quedar con distintas longitudes, hay que igualar las longitudes añadiendo 0's por la izquierda
-		//Primero hay que saber que numero es ek más largo
-		int a = 0; 
-		int m = 0;
-		int maxLongCaracter = 0;
-		for(int i = 0; i < codificacion.length-1; i++) {
-			a = codificacion[i].length();
-			m = codificacion[i+1].length();
-				
-			if(a > m) {
-				maxLongCaracter = a;
-			} else {
-				maxLongCaracter = m;
-			}		
-		}
-		
 		//Recorro el array de las codificaciones de la cadena
 		StringBuffer rellenarCeros = new StringBuffer();
 		for(int i = 0; i < codificacion.length; i++) {
-			int espaciosARellenar = maxLongCaracter - codificacion[i].length();
+			int espaciosARellenar = numEntero - codificacion[i].length();
 			
 			//Recorro hasta los espacios que hay que rellenar para poner 0's por la izquierda
 			for(int j = 0; j < espaciosARellenar; j++) {
@@ -198,7 +181,7 @@ public class sinRuido {
 		}
 		*/
 		
-		String[][] bloques = new String[5][1] ; //CAMBIAR A ARRAY PARA QUE NO HAYA PROBLEMAS CON LA LONGITUD
+		String[][] bloques = new String[32][1] ; //CAMBIAR A ARRAY PARA QUE NO HAYA PROBLEMAS CON LA LONGITUD
 		int numBloque = 0;
 		boolean faltanNumEnBloque = false;
 		for(int i = 0; i < cod1.length(); i+=n) {
@@ -289,9 +272,80 @@ public class sinRuido {
 	
 	//parte de decodificar sin ruido
 	public static void decodificar() {
-		
 		System.out.println("Prometo que más adelante decodificaré bien :)");
 		
+		System.out.println("Escriba la secuencia binaria a decodificar");
+		Scanner in = new Scanner(System.in);
+		
+		//Al contrario que en el caso anterior, aqui empezamos por la variable cod2
+		String cod2 = in.nextLine();
+		
+		//Almacenar por bloques por la dimensión de la matriz k
+		ArrayList<String> bloques = new ArrayList<String>();
+		
+		for(int i = 0; i < cod2.length(); i+=numEntero+1) {
+			bloques.add(cod2.substring(i, i+numEntero+1));
+		}
+		System.out.println(k);
+		System.out.println(numEntero);
+		System.out.println("BLOQUES");
+		for(int i = 0; i < bloques.size(); i++) {
+			System.out.println(bloques.get(i));
+		}
+		
+		//Este paso es en el que en el paso anterior multiplicabamos por la matriz
+		//Ahora, con quedarnos con los primeros n (otra dimension de la matriz) numeros nos basta
+		//Lo almacenamos en cod1
+		StringBuffer cod1 = new StringBuffer();
+		for(int i = 0; i < bloques.size(); i++) {
+			cod1.append(bloques.get(i).substring(0, 4));
+		}
+		
+		System.out.println("\nCOD1");
+		System.out.println(cod1.toString());
+		
+		//De COD1 tenemos que pasar a la codificación de las palabras
+		//Voy a probar a cojer k-1 numeros
+		ArrayList<String> codificaciones = new ArrayList<String>();
+		for(int i = 0; i < cod1.length(); i+=numEntero) {
+			//Si la cadena sigue pero nos pasamos, eso es que esos numeros sobran
+			if((i+k-1) > cod1.length()) {
+				break;
+			}
+			codificaciones.add(cod1.substring(i, i+numEntero));
+		}
+		
+		System.out.println("\nCODIFICACION LETRAS");
+		for(int i = 0; i < codificaciones.size(); i++) {
+			System.out.println(codificaciones.get(i));
+		}
+		
+		//Decodificar las posiciones de codificaciones y almacenarlas en un array
+		//Como en el anterior caso a la posicion le restábamos 1, ahora habria que sumarselo
+		//Pero como más adelante vamos a empezar a contar desde la posicon 0, no hace falta sumarle 1
+		ArrayList<String> posiciones = new ArrayList<String>();
+		int pos = 0;
+		for(int i = 0; i < codificaciones.size(); i++) {
+			pos = Integer.parseInt(codificaciones.get(i),2);
+			posiciones.add(String.valueOf(pos));
+		}
+		
+		for(int i = 0; i < posiciones.size(); i++) {
+			System.out.println(posiciones.get(i));
+		}
+		
+		//Como ya tenemos las posiciones, ahora hay que encontrar la letra con respecto al alfabeto
+		StringBuffer result = new StringBuffer();
+		for(int i = 0; i < posiciones.size(); i++) {
+			for(int j = 0; j < alfabeto.length(); j++) {
+				if(Integer.parseInt(posiciones.get(i)) == j) { //misma posicion, cogemos letra
+					result.append(alfabeto.charAt(j));
+				}
+			}
+		}
+		
+		System.out.println("RESULTADO");
+		System.out.println(result.toString());
 	}
 
 }
