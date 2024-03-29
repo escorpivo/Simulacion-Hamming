5a7c5be6c2559e64a2a9bc1586c3822f9d83f7a4
LorenaAlbillos <lalbim00@estudiantes.unileon.es>
2024-03-29 23:13:58 +0100

    update: modificacion para que coja los datos desde el main principal

--- src/sinRuido.java
+++ src/sinRuido.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 import java.util.Scanner;
 import java.math.*;
 
@@ -10,82 +11,15 @@ public class sinRuido {
 	static int n;
 	static int k;
 	static int numEntero = 0;
-	public static void main(String[] args) {
-		
-		//Declaramos variables globales:
-		n = 4;
-		k = 7;
-		matriz = new int[n][k];
-		alfabeto = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ abcdefghiíjklmnñopqrstuvwxyz";
-		
-		for(int i = 0 ; i < matriz.length; i ++) {
-			for(int j = 0; j < matriz[i].length; j++) {
-			
-				if(i == j) {
-					matriz[i][j] = 1;
-				}else {
-					matriz[i][j] = 0;
-				}
-				
-			}
-		}
-		
-		matriz[0][5]=1;
-		matriz[0][6]=1;
-		matriz[1][4]=1;
-		matriz[1][6]=1;
-		matriz[2][4]=1;
-		matriz[2][5]=1;
-		matriz[3][4]=1;
-		matriz[3][5]=1;
-		matriz[3][6]=1;
-		
-		System.out.println("MATRIZ");
-		for(int i = 0; i < matriz.length; i++) {
-			for(int j = 0; j <matriz[i].length; j++) {
-				System.out.print(matriz[i][j]);
-			}
-			System.out.println();
-		}
-		
-		//COD1 -- hacer el logaritmo en base 2 de la longitud del alfabeto
-		longitud = alfabeto.length();
-		double logaritmo = Math.log10(longitud)/Math.log10(2);
-		numEntero = (int) Math.round(logaritmo);
-		System.out.println("LOGARITMO Y SU ENTERO");
-		System.out.println(logaritmo);
-		System.out.println(numEntero);
-		
-		Scanner input = new Scanner(System.in);
-		System.out.println("1: Codificar		2: Decodificar");
-		
-		int eleccion = 0;
-		try {
-			
-			eleccion = input.nextInt();
-
-		} catch (Exception e) {
-
-			System.err.println("Error al elegir opcion");
-			return;
-		}
-		
-		switch (eleccion) {
-		case 1:
-			codificar();
-			break;
-
-		case 2:
-			decodificar();
-			break;
-			
-		default:
-			System.out.println("Chao");
-			return;
-		}
-		
-	}
 	
+	public sinRuido(int [][] matriz, int n, int k, int numEntero, int longitud, String alfabeto) {
+		this.matriz = matriz;
+		this.n= n;
+		this.k = k;
+		this.numEntero = numEntero;
+		this.longitud = longitud;
+		this.alfabeto = alfabeto;
+	}
 	
 	//parte de codificar sin ruido
 	public static void codificar() {
@@ -356,6 +290,10 @@ public class sinRuido {
 		
 		System.out.println("RESULTADO");
 		System.out.println(result.toString());
+		
+		
+		
+		
 	}
 
 }
